9ed67819c90509d18f24b3f113b5501418bc491a
Ethan Storm <stormethan10@gmail.com>
2022-10-25 19:40:32 -0700

    attempt to integreate socket for a chat app

--- AfterBreak/firstProject/server.js
+++ AfterBreak/firstProject/server.js
@@ -1,14 +1,36 @@
+// include and create express server
 const express = require('express')
 const app = express()
 const port = 3000
 
+// include and Create socket.io server 
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+
+// allow single folder to contain data
 app.use(express.static('public'))
+// allow express templates to be used
 app.set('view engine', 'ejs')
 
+
+io.on('connection', (socket) => {
+    console.log("hello World!!!")
+})
+// home page
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+// set chat room router
+const chat = require('./routes/chat')
+app.use('/chat',  chat)
+
+
+
+// listen for req and res
 app.listen(port, (req, res) => {
     console.log("Listening on port: ", port)
 })
