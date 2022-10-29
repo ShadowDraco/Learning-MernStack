03826a3a3cb68282cb75b4b90bb4b484aa396327
Ethan Storm <stormethan10@gmail.com>
2022-10-28 19:34:50 -0700

    made things look a little prettier

--- AfterBreak/secondProject/public/scripts/bible.js
+++ AfterBreak/secondProject/public/scripts/bible.js
@@ -5,7 +5,7 @@ if (apiBooks) {
     let books = apiBooks.data
  
     // 66 books in the bible
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 11; i++) {
         bookList.insertAdjacentHTML('beforeend', `<div class='bg-secondary row bookRow'></li>`)
     }
 
@@ -13,7 +13,7 @@ if (apiBooks) {
     let count = 0
     bookRows.forEach(row => { 
         for (let j = 0; j < 11; j++) {
-            bookRows[count].insertAdjacentHTML('beforeend', `<div class='col bg-secondary bg-gradient'> ${books[j + 11 * count].name} </li>`)
+            bookRows[count].insertAdjacentHTML('beforeend', `<div class='col bg-gradient text-center p-3 list-group-item list-group-item-dark'> ${books[j + 11 * count].name} </li>`)
             console.log('c')
         }
         count++
