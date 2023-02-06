e9f691993bba28bc50e7e80318f0a49e569fbe47
StormyFrolic <stormethan10@gmail.com>
2023-02-06 12:07:17 -0800

    Update server.js

--- AfterBreak/thirdProject/praiseAdonai/server.js
+++ AfterBreak/thirdProject/praiseAdonai/server.js
@@ -5,15 +5,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
 // use a static folder rather than an absolute path
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-
 // get the home page
 app.get('/', (req, res) => {
-    res.render('index', { motd: "Today is the first day of launch!", pageTitle: "Welcome to Praise Adonai!"})
+	res.render('index', {
+		motd: 'Today is the first day of launch!',
+		pageTitle: 'Welcome to Praise Adonai!',
+	})
 })
 
 // include the bible route
@@ -23,13 +24,19 @@ app.use('/bible', bibleRoute)
 
 // other routes unimplemented
 app.get('/music', (req, res) => {
-    res.render('music', { motd: "We sing holy holy holy!", pageTitle: "Find good music!"})
+	res.render('music', {
+		motd: 'We sing holy holy holy!',
+		pageTitle: 'Find good music!',
+	})
 })
 app.get('/faith', (req, res) => {
-    res.render('faith', { motd: "Faith the size of a mustard seed!", pageTitle: "Learn about The Faith!"})
+	res.render('faith', {
+		motd: 'Faith the size of a mustard seed!',
+		pageTitle: 'Learn about The Faith!',
+	})
 })
 
 // listen on the port for req res cycle
 app.listen(port, (req, res) => {
-    console.log('Server listening on port:'.blue, `${port}`.white)
+	console.log('Server listening on port:'.blue, `${port}`.white)
 })
